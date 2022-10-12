e8ada1b9e07539b91f62cee77f5229a381530666
Benjamin Bertin <benjamin@wawawum.com>
2022-10-11 22:35:04 -0400

    Loading

---
+++ PcfRedirect/PcfRedirectLoading.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export function PcfRedirectLoading(): JSX.Element {
+
+    return <div id="www-pcfredirect-container">
+        <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+        </div>
+    </div>
+
+}
--- PcfRedirect/index.ts
+++ PcfRedirect/index.ts
@@ -1,14 +1,17 @@
-import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
 import DataSetInterfaces = ComponentFramework.PropertyHelper.DataSetApi;
 type DataSet = ComponentFramework.PropertyTypes.DataSet;
+import { PcfRedirectLoading } from './PcfRedirectLoading';
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
 
 export class PcfRedirect implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+    private _container: HTMLDivElement;
 
     /**
      * Empty constructor.
      */
-    constructor()
-    {
+    constructor() {
 
     }
 
@@ -20,9 +23,8 @@ export class PcfRedirect implements ComponentFramework.StandardControl<IInputs, 
      * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
      * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
      */
-    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement): void
-    {
-        // Add control initialization code
+    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement): void {
+        this._container = container;
     }
 
 
@@ -30,12 +32,13 @@ export class PcfRedirect implements ComponentFramework.StandardControl<IInputs, 
      * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
      * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
      */
-    public updateView(context: ComponentFramework.Context<IInputs>): void
-    {
-        // Add code to update control view
+    public updateView(context: ComponentFramework.Context<IInputs>): void {
         const url = context.parameters.url.raw;
-        if(url !== null) {
-            window.location.assign(url);
+        if (url !== null) {
+            window.setTimeout(() => { window.location.assign(url) }, 500);
+
+            const root = createRoot(this._container);
+            root.render(React.createElement(PcfRedirectLoading));
         }
     }
 
@@ -43,8 +46,7 @@ export class PcfRedirect implements ComponentFramework.StandardControl<IInputs, 
      * It is called by the framework prior to a control receiving new data.
      * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
      */
-    public getOutputs(): IOutputs
-    {
+    public getOutputs(): IOutputs {
         return {};
     }
 
@@ -52,8 +54,7 @@ export class PcfRedirect implements ComponentFramework.StandardControl<IInputs, 
      * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
      * i.e. cancelling any pending remote calls, removing listeners, etc.
      */
-    public destroy(): void
-    {
+    public destroy(): void {
         // Add code to cleanup control if necessary
     }
 
